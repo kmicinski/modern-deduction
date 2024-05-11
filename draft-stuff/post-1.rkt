b8426aa776a3a157f3eaea41490ef3a2c74de522
Kristopher Micinski <krismicinski@gmail.com>
2024-05-11 15:05:14 -0400

    post1

--- draft-stuff/post-1.rkt
+++ draft-stuff/post-1.rkt
@@ -1,15 +1,37 @@
+;; Relational algebra
 #lang racket
 
-(define relation-name? symbol?)
+(define relation? symbol?)
+(define atom? symbol?) ;; atomic lits are symbols
+;; variables must be explicitly tagged, not 'x, but '(var x)
+(define (var? x) (match x [`(var ,(? symbol? x)) #t] [_ #f]))
+(define (var-or-atom? x) (or (atom? x) (var? x)))
+(define (fact? f)
+  (match f
+    [`((,(? relation? r) ,(? atom?) ...)) #t]
+    [_ #f]))
+(define (rule? r)
+  (match r
+    [`((,(? relation? head-rel) ,(? var-or-atom?) ...) <-- 
+       (,(? relation? body-rels) ,(? var-or-atom?) ...) ...)
+     #t]
+    [_ #f]))
+
+;; should all be true
+(rule? '((p) <--))
+(rule? '((r) <-- (q)))
+(rule? '((path x y) <-- (edge x x)))
+(rule? '((path x y) <-- (edge x y) (path y z)))
 
 (define (query? q)
-  (define (condition-size? R)
-    #t) ;; xxx
   (match q
     ;; scan a relation
-    [`(scan ,(? relation-name? R)) #t]
-    [`(select from ,(? relation-name? R) where ,(? (condition-size? R))) #t]
-    ;; natural join on the first N columns
+    [`(scan ,(? relation? R)) #t]
+    [`(select from ,(? relation? R)
+              where column ,(? nonnegative-integer? n)
+              equals ,(? atom?))
+     #t]
+    ;; natural join on the first N columns, values must be equal
     [`(,(? query? q0) ⋈ ,(? query? q1) on first ,(? nonnegative-integer? N)) #t]
     [`(,(? query? q0) ∪ ,(? query? q0)) #t]
     [`(,(? query? q0) ∩ ,(? query? q0)) #t]
@@ -22,22 +44,61 @@
     [_ #f]))
 
 ;; q: query?
-;; database: hashmap from relation name ↦ ℘(Tuple)
+;; db: hashmap from relation name ↦ ℘(Tuple)
 ;; universe: ℘(Tuple)
-(define (interpret-query q database universe)
-  (define (interpret-conditions conditions R)
-    (match conditions
-      ['() (hash-ref database R)]
-      [`([= ,n ,v] . ,rst-conditions)
-       (filter (lambda (tuple) (equal? (list-ref tuple n) v)) 
-               (interpret-conditions rst-conditions))]))
+(define (interpret-query q db universe)
   (match q
-    [`(scan ,R) (hash-ref database R)]
-    [`(select from ,R where ,condition)
-     (filter (interpret-condition condition)
-             (hash-ref database R))]
-    [`(,q0 ∪ ,q1) (set-union (interpret-query q0) (interpret-query q1))]
-    [`(,q0 ∩ ,q1) (set-intersection (interpret-query q0) (interpret-query q1))]
-    [`(,q0 ⋈ ,N ,q1)
-     (foldl
-      (foldl ))]))
+    [`(scan ,R) (hash-ref db R)]
+    [`(select from ,R where ,n equals ,k)
+     (filter (lambda (x) (equal? (list-ref x n) k))
+             (hash-ref db R))]
+    [`(,q0 ∪ ,q1) (set-union (interpret-query q0 db universe) (interpret-query q1 db universe))]
+    [`(,q0 ∩ ,q1) (set-intersect (interpret-query q0 db universe) (interpret-query q1 db universe))]
+    [`(,q0 ⋈ ,q1 on first ,N)
+     (list->set
+      (foldl
+       (λ (t0 tups)
+         (foldl (λ (t1 tups)
+                  (if (equal? (take t0 N) (take t1 N))
+                      (set-add tups (append (take t0 N) (drop t0 N) (drop t1 N)))
+                      tups))
+                tups
+                (set->list (interpret-query q1 db universe))))
+       (set)
+       (set->list (interpret-query q0 db universe))))]
+    [`(reorder ,q ,order ...)
+     (let ([ts (set->list (interpret-query q db universe))])
+       (set->list
+        (map (λ (t) (map (λ (i) (list-ref t i)) order))
+             ts)))]
+    [`(project ,(? query? q) ,(? nonnegative-integer? n))
+     (list->set (map (λ (t) (take t n)) (set->list (interpret-query q db universe))))]
+    ;; need closed-world assumption
+    [`(- ,(? query? q))
+     (set-subtract universe (interpret-query q db universe))]))
+
+;; relations: hash from relation names to their associated arities
+;; herbrand-base is a list of atoms 
+(define (herbrand-universe relations herbrand-base)
+  (foldl
+   (λ (R db)
+     (hash-set db
+               R 
+               (set->list
+                (foldl
+                 (λ (i lists)
+                   (map (lambda (list) (map (lambda (i) (cons i list)) herbrand-base))
+                        lists))
+                 '(())
+                 (range (hash-ref relations R))))))
+   (hash)
+   (hash-keys relations)))
+
+(herbrand-universe (hash 'R 2 'K 1 'Q 3) '(a b c d e))
+
+(interpret-query '(- ((scan R) ⋈ (scan K) on first 1)) 
+                 (hash 'K (list->set '((1 2) (3 4)))
+                       'R (list->set '((1 2) (2 3) (3 4) (2 5) (3 1)))))
+
+
+
